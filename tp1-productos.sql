48a46afeacc51fea7bd6504962421c73fe01c8aa
Giuliana Cagnola <98410646+GiuliCagnola@users.noreply.github.com>
2024-11-17 11:47:44 -0300

    Update tp1-productos.sql

--- tp1-productos.sql
+++ tp1-productos.sql
@@ -1,19 +1,21 @@
 ---------- TP1 ----------
 --*--*--*--*--*--*--*--*--
 
------ESQUEMA PRODUCTO-----
--------------------------
+----- ESQUEMA PRODUCTO -----
+----------------------------
 
 ----- Productos con información de marca, categoría y proveedor
-
-select 
-	p.descripcion as nombre_producto,
-	m.descripcion as nombre_marca,
-	c.descripcion as categoria
-from producto p 
-join marca m on m.id = p.id_marca --unión entre producto y marca
-join subcategoria s on s.id = p.id_subcategoria --unión entre producto y subcategoria 
-join categoria c on c.id = s.id_categoria --unión entre subcategoria y categoria
-order by nombre_marca, nombre_producto, categoria;
+SELECT 
+    p.descripcion AS nombre_producto,
+    m.descripcion AS nombre_marca,
+    c.descripcion AS categoria
+FROM producto p
+JOIN marca m ON m.id = p.id_marca -- Unión entre producto y marca
+JOIN subcategoria s ON s.id = p.id_subcategoria -- Unión entre producto y subcategoría
+JOIN categoria c ON c.id = s.id_categoria -- Unión entre subcategoría y categoría
+ORDER BY 
+    nombre_marca, 
+    nombre_producto, 
+    categoria;
 
 
